22eed75e2b6581a601fa0e9df1f683e96722b570
jp@jp-embedded.com <jp@jp-embedded.com@494deab6-d6fb-9404-d9d9-0bf10ae0562c>
2014-04-03 11:51:19 +0000

    initial import

---
+++ src/test.cpp
@@ -0,0 +1,21 @@
+#include "syslog.h"
+
+int main(int argc, char *argv[])
+{
+	// write to syslog
+	syslog::ostream log;
+	log << syslog::level::info << "info message 1" << std::endl;
+	log << "info message 2" << std::endl;
+
+	// redirect cout, cerr and clog to syslog
+	syslog::redirect cout_redir(std::cout);
+	syslog::redirect cerr_redir(std::cerr);
+	syslog::redirect clog_redir(std::clog);
+
+	std::cout << "cout message" << std::endl;
+	std::clog << "clog message" << std::endl;
+	std::cerr << "cerr message" << std::endl;
+
+	return 0;
+}
+
